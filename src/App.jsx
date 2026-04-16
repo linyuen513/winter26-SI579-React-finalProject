88f6bc26c3ed8052b1670e19d164aa440ab6038f
AnneLin <annelovedance0513@gmail.com>
2026-04-16 00:31:19 -0400

    add count 3 times

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,12 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Anne Lin</h1>
-          <h2>linyuen</h2>
+          <h1>Hello This is Anne Lin</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
+          <p>name: Anne Lin</p>
+          <p>uniqname: linyuen</p>
         </div>
         <button
           className="counter"
@@ -28,6 +29,7 @@ function App() {
         >
           Count is {count}
         </button>
+        <p>Count 3 times: {count*3}</p>
       </section>
 
       <div className="ticks"></div>
